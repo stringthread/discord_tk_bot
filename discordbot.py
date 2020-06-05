85ea277700819e2a41887c5fd35d5c9f31cd82e6
stringthread <string.thread@gmail.com>
2020-06-06 00:05:40 +0900

    Sound function

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,16 @@ import asyncio
 
 bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
+if not discord.opus.is_loaded(): 
+    discord.opus.load_opus("heroku-buildpack-libopus")
+se_start=discord.FFmpegPCMAudio("audio/start.mp3")
+se_fin=discord.FFmpegPCMAudio("audio/fin.mp3")
 
+async def se(vc_list,src):
+    for ch in vc_list:
+        v_client=await ch.connect()
+        v_client.play(src)
+        await v_client.disconnect()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -16,6 +25,8 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def t(ctx,arg):
+    global se_start
+    global se_fin
     if not(arg.isdecimal()):
         await ctx.send('Error: invalid time.')
         return
@@ -24,9 +35,14 @@ async def t(ctx,arg):
         sec=sec//100*60+sec%100
     else:
         sec*=60
+    ch=ctx.channel
+    cat=ctx.guild.get_channel(ch.category_id)
+    vc_list=cat.voice_channels
+    await se(vc_list,se_start)
     await ctx.send(f"Timer set: {sec}")
     await asyncio.sleep(sec)
     await ctx.send('Finished!')
+    await se(vc_list,se_fin)
 
 
 bot.run(token)
